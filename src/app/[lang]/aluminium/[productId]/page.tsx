ff4ac60af53aabdc06fb68eefdac6e2682f8d42f
Mark Daniliuk <daniliukmark@gmail.com>
2024-01-13 17:16:52 +0100

    deleted page

--- src/app/[lang]/aluminium/[productId]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface Page {
-  lang: string;
-  params: {
-    productId: string;
-  };
-}
-export default function Page({ lang, params: { productId } }: Page) {
-  return <h1>{productId}</h1>;
-}
